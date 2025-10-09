dfe623fb2c22c9bafdf2bbc5caa6ca51d6fb6c4d
August <117049354+Auggie0w0@users.noreply.github.com>
2025-10-08 17:59:43 -0700

    clean up

---
+++ vite-project/src/nav.jsx
@@ -0,0 +1,12 @@
+function Nav() {
+    return (
+        <nav>
+            <h1>WEBSITE</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
